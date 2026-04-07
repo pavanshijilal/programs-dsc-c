5ed55ba625683dbb26a8442e3e372f46980b42b3
youknowwho <slalpavan8@gmail.com>
2026-04-07 07:44:54 +0530

    Update pattern_loop.c

--- pattern_loop.c
+++ pattern_loop.c
@@ -1,7 +1,30 @@
 /*Pavan S Lal
 ISP*/
 #include<stdio.h>
+void pattern1();
+void pattern2();
 int main()
+{
+    int choice;
+    do
+    {
+        printf("\nEnter choice:\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:pattern1();
+                   break;
+            case 2:pattern2();
+                   break;
+            case 3:printf("\nThank you for using the program:\n");
+                   break;
+            default:printf("\nWrong choice");
+            
+        }
+    }while(choice!=3);
+    return 0;
+}
+void pattern1()
 {
     int i,j;
     for(i=1;i<=4;i++)
@@ -13,5 +36,17 @@ int main()
         printf("\n");
         
     }
-    return 0;
+}
+void pattern2()
+{
+    int num=1;
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d\t",num++);
+        }
+        printf("\n");
+    }
 }
